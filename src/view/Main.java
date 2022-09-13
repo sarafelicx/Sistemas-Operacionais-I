93817a7b119d0552d16336d8e84193afdfe84e0f
sarafelicx <111999452+sarafelicx@users.noreply.github.com>
2022-09-13 16:49:56 -0300

    Ex1 Threads

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,17 +1,24 @@
 package view;
 
-import controller.IdThread;
+import controller.ThreadVetor;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-	
-		for (int idThread = 0; idThread < 5; idThread++) {
-			
-			Thread id = new IdThread(idThread);
-			id.start();
+		int[] vetor = new int[1000];
+		int tamanhoVetor = vetor.length;
 
+		for (int i = 0; i < tamanhoVetor; i++) {
+
+			vetor[i] = (int) ((Math.random() * 101) + 1);
 		}
+
+		for (int valor = 1; valor < 3; valor++) {
+
+			Thread threadVetor = new ThreadVetor(vetor, valor);
+			threadVetor.start();
+		}
+
 	}
 }
